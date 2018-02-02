5bf801c6692df8f7fcc180ef8704a714f05555f1
Miwa <paintfx@gmail.com>
2018-02-02 21:12:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :name. :grade
+  attr_accessor :name, :grade
   attr_reader :id
 
   def self.create_table
