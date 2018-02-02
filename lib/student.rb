d14669fd30d7170c7e3d90aa97437b5437291023
Miwa <paintfx@gmail.com>
2018-02-02 21:13:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,12 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
